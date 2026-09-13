7ce5d5c8177607b46ac80ad7b3061fcafa5cf415
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-09-13 17:57:33 +0200

    fix: remove trailing slash from base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/araDine/",
+  base: "/araDine",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
