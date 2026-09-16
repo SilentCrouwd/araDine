b852884da110e5869c42425e4992fd1b5f6011f5
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-09-16 18:24:47 +0200

    feat: enhance RefreshmentDetail component with detailed layout and dynamic data handling

--- src/Views/Dashboard/DashboardViews/DashboardEmployee.tsx
+++ src/Views/Dashboard/DashboardViews/DashboardEmployee.tsx
@@ -20,6 +20,19 @@ import {
 } from "@/components/ui/select";
 import RefreshmentCard from "./components/RefreshmentCard";
 import { Link } from "react-router";
+
+function handleRoomStatus(roomStatus: string) {
+  if (roomStatus === "Belegt") {
+    return "bg-red-500";
+  } else if (roomStatus === "Abräumen") {
+    return "bg-blue-500";
+  } else if (roomStatus === "Service") {
+    return "bg-yellow-500";
+  } else {
+    return "bg-green-500";
+  }
+}
+
 function DashboardEmployee() {
   const items = [
     { label: "LHM", value: "LHM" },
@@ -29,9 +42,12 @@ function DashboardEmployee() {
   const roomOverview = [
     { room: "214", floor: "1. Stock", startTime: "12:00", status: "Belegt" },
     { room: "215", floor: "1. Stock", startTime: "13:00", status: "Frei" },
-    { room: "216", floor: "1. Stock", startTime: "14:00", status: "Belegt" },
+    { room: "217", floor: "1. Stock", startTime: "14:00", status: "Abräumen" },
+    { room: "216", floor: "1. Stock", startTime: "14:00", status: "Service" },
   ];
+
   const [date, setDate] = useState<Date>();
+  const [open, setOpen] = useState(false);
   return (
     <div className="flex flex-col py-4 px-4 gap-4  ">
       <div className="  grid grid-cols-2 self-center md:gap-5">
@@ -52,7 +68,7 @@ function DashboardEmployee() {
             </SelectGroup>
           </SelectContent>
         </Select>
-        <Popover>
+        <Popover open={open} onOpenChange={setOpen}>
           <PopoverTrigger
             render={
               <Button
@@ -74,7 +90,14 @@ function DashboardEmployee() {
             <CalendarIcon className="text-muted-foreground" />
           </PopoverTrigger>
           <PopoverContent className="w-auto p-0">
-            <Calendar mode="single" selected={date} onSelect={setDate} />
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={(date) => {
+                setDate(date);
+                setOpen(false);
+              }}
+            />
           </PopoverContent>
         </Popover>
       </div>
@@ -169,15 +192,21 @@ function DashboardEmployee() {
           </div>
           {roomOverview.map((room) => (
             <div
-              className="grid grid-cols-4 border-b border-border  py-0.5   "
+              className="grid grid-cols-4 border-b border-border content-center items-center py-0.5   "
               key={room.room}
             >
               <p>{room.room}</p>
               <p>{room.startTime}</p>
-              <p>{room.status}</p>
-              <Button className="w-fit " variant="outline">
-                edit
-              </Button>
+              <p
+                className={`${handleRoomStatus(room.status)} flex justify-center  w-1/2 font-bold text-lg rounded-xl text-muted-foreground text-shadow-sm/40 `}
+              >
+                {room.status}
+              </p>
+              <Link to={`araDine/dashboard/refreshment-detail/${"2"}`}>
+                <Button className="w-fit " variant="outline">
+                  edit
+                </Button>
+              </Link>
             </div>
           ))}
         </div>
